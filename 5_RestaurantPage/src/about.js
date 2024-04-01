2af2f2f6ee650d0d43b57726f8bc2ad426a0408c
Alan Napieralski <alan.napieralski03@gmail.com>
2024-04-01 15:59:45 +0100

    started working on the todo list app

--- 5_RestaurantPage/src/about.js
+++ 5_RestaurantPage/src/about.js
@@ -1,5 +1,3 @@
-
-
 export default function aboutComponent() {
     const container = document.createElement('div');
 
---
+++ 6_ToDoApp/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map', // good for illustrative purposes (though not for production)
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html', // using index.html as a template for the new project
+        title: 'Todo App', // set html doc title
+        filename: 'index.html', // set file name (other than index.html)
+        inject: 'body', // injects the script tag (at the bottom of the body tag)
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
