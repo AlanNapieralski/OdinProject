6e9333045b1b1a1642c7aa1f3eb23d3020b0fc43
AlanNapieralski <122971912+AlanNapieralskiUni@users.noreply.github.com>
2024-05-31 19:49:27 +0000

    Finished the CV application using react useState and props with importance on reusing components

--- 12_CV-Application-react/src/components/GeneralInfo.jsx
+++ 12_CV-Application-react/src/components/GeneralInfo.jsx
@@ -1,9 +1,16 @@
+import Section from './Section'
+
 function GeneralInfo() {
 
+    const name = 'General Information'
+    const fields = [
+        {name: 'First Name', type: 'text'},
+        {name: 'Last Name', type: 'text'}, 
+        {name: 'Email', type: 'email'},
+        {name: 'Phone Number', type: 'tel'}
+    ]
 
-    return (
-        <>
-            
-        </>
-    )
+    return <Section name={name} fields={fields} />
 }
+
+export default GeneralInfo
--- 12_CV-Application-react/src/components/Section.jsx
+++ 12_CV-Application-react/src/components/Section.jsx
@@ -9,8 +9,14 @@ function Section(props) {
         }, {})
     )
 
-    const handleSubmit = () => {
+    const [edit, setEdit] = useState(0)
 
+    const handleSubmit = () => {
+       setEdit( prevState => 1 - prevState)
+    }
+
+    const handleEdit = () => {
+       setEdit( prevState => 1 - prevState)
     }
 
     const handleChange = (name, value) => {
@@ -23,23 +29,28 @@ function Section(props) {
     const fieldsList = props.fields.map( field => {
         const {name, type} = field
 
-        return ( 
-            <div key={name}>
-                <h2>{name}</h2>
-                <input type={type} value={values[name]}
-                    onChange={ event => handleChange(name, event.target.value)}>
-                </input>
+        return (
+            <div key={name} className='flex justify-between items-center m-4 p-1 border-2 border-gray-500 border-opacity-50 rounded'>
+                <h2 className='font-bold pr-4'>{name}</h2>
+                {edit ? <input className='p-1 w-2/3 border-l-2 truncate' type={type} value={values[name]}
+                    onChange={ event => handleChange(name, event.target.value)}></input>
+                : <p className='p-1 w-2/3 border-l-2 border-gray-500 pl-4 text-left'>{values[name]}</p>}
             </div>
         )
     })
 
     return (
-        <div className='w-1200px p-4 border-4 rounded space-y-6 > *'>
-            <h1>{props.name}</h1>
-            <div className='space-y-2 > *'>
-              {fieldsList}
+        <div className='flex flex-col items-end mb-8'>
+            <div className='w-full p-4 border-4 rounded space-y-6 > *'>
+                <div className="flex justify-between p-4">
+                    <h1 className='text-left'>{props.name}</h1>
+                    { edit ? null : <button onClick={handleEdit} className='text-right'>Edit</button>}
+                </div> 
+                <div>
+                    {fieldsList}
+                </div>
             </div>
-            <button onClick={handleSubmit}>Submit</button>
+            { edit ? <button onClick={handleSubmit} className='m-4 bg-green-500'>Submit</button> : null }
         </div>
     )
 }
