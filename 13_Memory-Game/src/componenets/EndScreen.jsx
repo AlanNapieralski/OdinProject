5b7c8b1ea91a0b6b484995d8218bd641b2f25664
Alan Napieralski <a.napieralski@lancaster.ac.uk>
2024-07-28 23:27:51 +0100

    Started learning Nodejs

--- 13_Memory-Game/src/componenets/EndScreen.jsx
+++ 13_Memory-Game/src/componenets/EndScreen.jsx
@@ -1,15 +1,15 @@
 import Restart from './Restart.jsx'
 import './EndScreen.css'
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 import useSound from 'use-sound'
 import tetteree from '../soundEffects/tetteree.mp3'
 import sad from '../soundEffects/sad.mp3'
 
-export default function EndScreen({best, score, result, setResult, setScore}) {
-   
+export default function EndScreen({ best, score, result, setResult, setScore }) {
+
     const endTitle = () => result.win ? 'You Won!!!' : 'You lost :o'
-    const [playWin, {stop: stopWin}] = useSound(tetteree)
-    const [playDefeat, {stop: stopLoss}] = useSound(sad)
+    const [playWin, { stop: stopWin }] = useSound(tetteree)
+    const [playDefeat, { stop: stopLoss }] = useSound(sad)
 
     if (result.win)
         playWin()
@@ -23,7 +23,7 @@ export default function EndScreen({best, score, result, setResult, setScore}) {
 
     return (
         <div className='flex justify-center items-center absolute w-full h-full end-animate'>
-            <div className='flex flex-col justify-center items-center p-12 w-1/2 h-3/4 bg-red-900 border-4 border-white rounded opacity-85'> 
+            <div className='flex flex-col justify-center items-center p-12 w-1/2 h-3/4 bg-red-900 border-4 border-white rounded opacity-85'>
                 <h1 className='font-sans text-6xl text-white '>{endTitle()}</h1>
                 <p className='font-sans text-3xl text-white '>Current score: {score}</p>
                 <p className='font-sans text-3xl text-white '>Best score: {best}</p>
